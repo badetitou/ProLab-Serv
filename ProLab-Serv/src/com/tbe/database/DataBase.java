77c6469d69fdc3f2fff0ac9a28f2645bcaaded7a
Benoit Verhaeghe <badetitou@gmail.com>
2015-03-04 10:56:11 +0100

    Simple user table.. database started. Don't push database to avoid problem

--- ProLab-Serv/src/com/tbe/database/DataBase.java
+++ ProLab-Serv/src/com/tbe/database/DataBase.java
@@ -4,18 +4,39 @@ import java.sql.*;
 
 public class DataBase {
 
-	private Connection c;
-	
+	private static Connection c;
+	private static final Integer version = 1;
+
+	private static String strCreateUserTable = "Create table users (username char(20), password char(20), firstname char(20), surname char(20));";
+
 	public DataBase() {
+		System.out.println("Init BDD...");
 		Connection c = null;
 		try {
 			Class.forName("org.sqlite.JDBC");
 			c = DriverManager.getConnection("jdbc:sqlite:prolab.db");
+			DataBase.c = c;
+			createTable();
 		} catch (Exception e) {
 			System.err.println(e.getClass().getName() + ": " + e.getMessage());
 			System.exit(0);
 		}
-		this.c = c;
 		System.out.println("Opened database successfully");
+	}
+
+	private void createTable() {
+		System.out.println("Init Table");
+		try {
+			Statement stmt;
+			stmt = DataBase.c.createStatement();
+			stmt.executeUpdate(strCreateUserTable);
+		} catch (Exception e) {
+			System.out.println("Table user already exist... No problem");
+		}
+		System.out.println("Init Table Done");
+	}
+
+	public static Connection getConnection() {
+		return c;
 	}
 }
--- ProLab-Serv/src/com/tbe/rest/Launcher.java
+++ ProLab-Serv/src/com/tbe/rest/Launcher.java
@@ -7,11 +7,14 @@ import javax.ws.rs.core.UriBuilder;
 import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
+import com.tbe.database.DataBase;
+
 public class Launcher {
 	public static void main(String[] args) {
 		URI baseUri = UriBuilder.fromUri("http://localhost/v1").port(9876).build();
 		ResourceConfig config = new ResourceConfig(UserREST.class);
 		JdkHttpServerFactory.createHttpServer(baseUri, config, true);
+		new DataBase();
 		System.out.println("Server started");
 	}
 }
