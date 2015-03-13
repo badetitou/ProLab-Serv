c60975c222df36c88106c2b1f9b7adc6cf3e072f
badetitou <badetitou@gmail.com>
2015-03-13 15:46:06 +0100

    Task ok

--- ProLab-Serv/src/com/tbe/database/TaskRequest.java
+++ ProLab-Serv/src/com/tbe/database/TaskRequest.java
@@ -1,5 +1,34 @@
 package com.tbe.database;
 
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.tbe.json.Task;
+
 public class TaskRequest {
 
+
+
+	public static List<Task> getAllTask() {
+		return new ArrayList<Task>();
+	}
+
+	public static String addProject(String username, String project,
+			String fonctionnality) {
+		String sql = "insert into tasks (username, fonctionnality, project) values ('" +
+				username + "'," +
+				fonctionnality + "," +
+				project + ");";
+		try {
+			Statement stmt = DataBase.getConnection().createStatement();
+			stmt.executeUpdate(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.err.println(sql);
+			return null;
+		}
+		return "ok";
+	}
 }
---
+++ ProLab-Serv/src/com/tbe/rest/TaskREST.java
@@ -0,0 +1,41 @@
+package com.tbe.rest;
+
+import java.util.List;
+
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+import com.tbe.database.ProjectsRequest;
+import com.tbe.database.TaskRequest;
+import com.tbe.json.Project;
+import com.tbe.json.Task;
+
+@Path("/task")
+public class TaskREST {
+	
+	@GET
+	public Task[] getAllTasks(){
+		System.out.println("GET ALL tasks");
+		List<Task> tasks = TaskRequest.getAllTask();
+		Task[] p = new Task[tasks.size()];
+		for (int i = 0;i<p.length;++i){
+			p[i] = tasks.get(i);
+		}
+		return p;
+	}
+	
+	@POST
+	public Response postProject(@FormParam("username") String username, @FormParam("idProject") String project, @FormParam("idFonctionnality") String fonctionnality){
+		System.out.println("Post Task");
+		String result =  TaskRequest.addProject(username, project, fonctionnality);
+		if (result == null){
+	        return Response.status(Response.Status.BAD_REQUEST).entity("Entity already exist").build();
+		}
+		return Response.status(Response.Status.CREATED).entity("Project Created").build();
+	}
+
+}
