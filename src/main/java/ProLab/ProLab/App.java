7357b0ba338bf420f1d498b1df3d2aa329d8c8b4
badetitou <badetitou@gmail.com>
2015-03-16 15:27:00 +0100

    Eclipse Project

---
+++ src/main/java/ProLab/ProLab/App.java
@@ -0,0 +1,13 @@
+package ProLab.ProLab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
