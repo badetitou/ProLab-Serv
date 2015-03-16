d679de68aa2de38d901436a889fccb7a944f505d
badetitou <badetitou@gmail.com>
2015-03-16 16:15:29 +0100

    Test AZAE 2

--- ProLab-Serv/README.md
+++
@@ -1,1 +0,0 @@
-# ProLab-Serv
---
+++ ProLab-Serv/src/test/java/fr/iutinfo/Test.java
@@ -0,0 +1,15 @@
+package fr.iutinfo;
+
+import org.glassfish.jersey.test.JerseyTest;
+
+public class Test extends JerseyTest {
+	/*
+	 * @Override protected Application configure() { return new App(); }
+	 * 
+	 * @Test public void getUsers () { int port = this.getPort(); String baseUrl
+	 * = "http://localhost:"+port+"/user/"; RestClient client = new
+	 * RestClient(); client.addUser(new User(0, "Thomas"), baseUrl );
+	 * client.addUser(new User(0, "Yann"), baseUrl ); List<User> users =
+	 * client.getUrlAsUser(baseUrl ); assertEquals(2, users.size()); }
+	 */
+}
