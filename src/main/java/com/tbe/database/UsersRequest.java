dc12b882015ea73c4d956ebd66bdc77ffdb04eaf
badetitou <badetitou@gmail.com>
2015-03-18 16:48:27 +0100

    UserRequest prepare statement

--- src/main/java/com/tbe/database/UsersRequest.java
+++ src/main/java/com/tbe/database/UsersRequest.java
@@ -1,5 +1,6 @@
 package com.tbe.database;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -12,10 +13,16 @@ public class UsersRequest {
 	
 	public static String addUser(String username, String password, String email,
 			String firstName, String surName) {
-		String sql = "Insert into users(username, password, email, firstname,surname) values ('"+ username+"','"+ password+ "','"+email+"','"+firstName+"','" + surName+"');";
+		String sql = "Insert into users(username, password, email, firstname,surname) values (?,?,?,?,?);";
 		try {
-			Statement stmt = DataBase.getConnection().createStatement();
-			stmt.executeUpdate(sql);
+			PreparedStatement stmt = DataBase.getConnection().prepareStatement(sql);
+			stmt.setString(1, username);
+			stmt.setString(2, password);
+			stmt.setString(3, email);
+			stmt.setString(4, firstName);
+			stmt.setString(5, surName);
+
+			stmt.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
 			System.err.println(sql);
@@ -41,10 +48,12 @@ public class UsersRequest {
 
 	public static User getUser(String username, String password) {
 		User user = null;
-		String sql = "Select * from users where username='" + username + "' and password='" + password + "';";
+		String sql = "Select * from users where username=? and password=?;";
 		try {
-			Statement stmt = DataBase.getConnection().createStatement();
-			ResultSet rs = stmt.executeQuery(sql);
+			PreparedStatement stmt = DataBase.getConnection().prepareStatement(sql);
+			stmt.setString(1, username);
+			stmt.setString(2, password);
+			ResultSet rs = stmt.executeQuery();
 			while (rs.next()){
 				user = new User(rs.getString("username"), rs.getString("password"), rs.getString("email"), rs.getString("firstname"), rs.getString("surname"));
 			}
