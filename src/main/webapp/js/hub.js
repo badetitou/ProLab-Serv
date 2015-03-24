1a82bd34a58bcca8c592cfe7efdf630473c81cfe
selenazelia <selena.zelia@gmail.com>
2015-03-24 17:30:09 +0100

    clean up 3

--- src/main/webapp/js/hub.js
+++ src/main/webapp/js/hub.js
@@ -1,26 +1,13 @@
-function getProject() {
-	var username = getCookie('log');
-	$
-			.getJSON(
-					"../v1/members/" + username,
-					function(data) {
-						var text = "";
-						for (var i = 0; i < data.length; ++i) {
-							text += "<li class='list-group-item'><span class='badge'><a onclick='addLike("
-									+ data[i].idi
-									+ ")' class='bg-primary'></span></a>      "
-									+ "<span class='text-primary' data-toggle='collapse' href='#"
-									+ i
-									+ "'>"
-									+ data[i].name
-									+ "</span>"
-									+ "<div class='collapse'  id='"
-									+ i
-									+ "'>"
-									+ "<span>"
-									+ data[i].punchline
-									+ "</span><br/>" + "</div></li></ul>";
-						}
-						document.getElementById("result").innerHTML = text;
-					});
-}
+var username = getCookie('log');
+$.getJSON("../v1/members/" + username, function(data) {
+		var text = "";
+		for (var i = 0; i < data.length; ++i) {
+			text += "<li class='list-group-item'><span class='badge'><a onclick='"
+			+ data[i].idi + ")' class='bg-primary'></span></a>"
+			+ "<span class='text-primary' data-toggle='collapse' href='#" + i + "'>"
+			+ data[i].name + "</span>"
+			+ "<div class='collapse'  id='" + i + "'>" + "<span>" + data[i].punchline
+			+ "</span><br/>" + "</div></li></ul>";
+		}
+		document.getElementById("result").innerHTML = text;
+	});
