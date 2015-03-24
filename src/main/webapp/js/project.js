9c324167e3b8b8610c5d8bd989b233908af16712
selenazelia <selena.zelia@gmail.com>
2015-03-24 17:36:53 +0100

    cleanup, j'ai que ca a foutre

--- src/main/webapp/js/project.js
+++ src/main/webapp/js/project.js
@@ -1,26 +1,28 @@
+/*
+	Create a new project
+*/
 function create() {
 	var name = $('#pro-name').val();
 	var description =  $('#pro-description').val();
 	var url = $('#pro-name').val();
 	var punchline =  $('#pro-punchline').val();
-		$.ajax({
-			type : 'POST',
-			contentType : 'application/json',
-			url : "v1/projects/" + name,
-			dataType : "json",
-			data : JSON.stringify({
-				"name" : name,
-				"description" : description,
-				"url" : url,
-				"punchline" :  punchline,
-			}),
-			success : function(data, textStatus, jqXHR) {
-					setCookie('project', url, 265);
-					alert('SetCookieFait');
-					window.location.href = "members.html"
-			},
-			error : function(jqXHR, textStatus, errorThrown) {
-				alert('FAIL');
-			}
-		});
-	}
+
+	$.ajax({
+		type : 'POST',
+		contentType : 'application/json',
+		url : "v1/projects/" + name,
+		dataType : "json",
+		data : JSON.stringify({
+			"name" : name,
+			"description" : description,
+			"url" : url,
+			"punchline" :  punchline,
+		}),
+		success : function(data, textStatus, jqXHR) {
+				setCookie('project', url, 265);
+				window.location.href = "members.html"
+		},
+		error : function(jqXHR, textStatus, errorThrown) {
+		}
+	});
+}
